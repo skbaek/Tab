70909a69464a8713412d640ac630e5e6ef4e43e8
skbaek <seulkeebaek@gmail.com>
2019-06-18 02:20:06 -0400

    Add comment

--- tab.lean
+++ tab.lean
@@ -1,35 +1,37 @@
+/- Propositional tableaux prover from Jeremy Avigad's lecture notes. -/
+
 open expr tactic classical
 
 variables {p q r s : Prop}
 variables {a b c d e : Prop}
 
-section 
+section
 
 local attribute [instance] classical.prop_decidable
-  
+
   theorem not_or_of_imp (h : a → b) : ¬ a ∨ b :=
   if ha : a then or.inr (h ha) else or.inl ha
-  
+
   theorem imp_iff_not_or : (a → b) ↔ (¬ a ∨ b) :=
   ⟨not_or_of_imp, or.neg_resolve_left⟩
-  
+
   theorem iff_def : (a ↔ b) ↔ (a → b) ∧ (b → a) :=
   iff_iff_implies_and_implies _ _
-  
+
   theorem not_not : ¬¬a ↔ a :=
   iff.intro by_contradiction not_not_intro
-  
+
   theorem not_or_distrib : ¬ (a ∨ b) ↔ ¬ a ∧ ¬ b :=
   ⟨λ h, ⟨λ ha, h (or.inl ha), λ hb, h (or.inr hb)⟩,
    λ ⟨h₁, h₂⟩ h, or.elim h h₁ h₂⟩
-  
+
   theorem not_and_of_not_or_not (h : ¬ a ∨ ¬ b) : ¬ (a ∧ b)
   | ⟨ha, hb⟩ := or.elim h (absurd ha) (absurd hb)
-  
+
   theorem not_and_distrib : ¬ (a ∧ b) ↔ ¬a ∨ ¬b :=
   ⟨λ h, if ha : a then or.inr (λ hb, h ⟨ha, hb⟩) else or.inl ha, not_and_of_not_or_not⟩
 
-end 
+end
 
 meta def normalize : tactic unit :=
 `[ try { simp only
@@ -124,21 +126,15 @@ example : (¬q → ¬p) → (p → q) := by tab
 example : p ∨ ¬p := by tab
 example : (((p → q) → p) → p) := by tab
 
-example (h₁ : a ∧ b) (h₂ : b ∧ ¬ c) : a ∨ c :=
-by tab
+example (h₁ : a ∧ b) (h₂ : b ∧ ¬ c) : a ∨ c := by tab
 
-example (h₁ : a ∧ b) (h₂ : b ∧ ¬ c) : a ∧ ¬ c :=
-by tab
+example (h₁ : a ∧ b) (h₂ : b ∧ ¬ c) : a ∧ ¬ c := by tab
 
-example : ((a → b) → a) → a :=
-by tab
+example : ((a → b) → a) → a := by tab
 
-example : (a → b) ∧ (b → c) → a → c :=
-by tab
+example : (a → b) ∧ (b → c) → a → c := by tab
 
 example (α : Type) (x y z w : α) :
-  x = y ∧ (x = y → z = w) → z = w :=
-by tab
+  x = y ∧ (x = y → z = w) → z = w := by tab
 
-example : ¬ (a ↔ ¬ a) :=
-by tab
+example : ¬ (a ↔ ¬ a) := by tab
